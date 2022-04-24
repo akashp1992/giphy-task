167bb660990a15cf4182ffa52d852c082cf5b541
akashprajapati <akashp35@gmail.com>
2022-04-24 17:19:16 +0530

    errorsolve

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import GlobalStyle from './GlobalStyle';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <Home/>
+      <Home />
     </>
   );
 }
